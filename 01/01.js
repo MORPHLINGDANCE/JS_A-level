0faeca55380fed37066eaca9accb465aac0e2e8c
MORPHLINGDANCE <145044270+MORPHLINGDANCE@users.noreply.github.com>
2024-05-25 21:39:43 +0300

    Add files via upload

---
+++ 01/01.js
@@ -0,0 +1,20 @@
+
+    // Calc
+    {
+        const dataAboutUser = ['Среднее значение часов сна на день:','Количество дней в месяце:'].map(prompt)
+        const totalSleepHoursInMonth = dataAboutUser[0] * dataAboutUser[1];
+        alert(`Общее количество часов потраченых на сон в месяц составляет: ${totalSleepHoursInMonth}`);
+    }
+    // CalcDebug
+    {
+        let firstValue = 5;
+        firstValue = 'Hello';
+        const globalValue = 123213123;
+        console.log(globalValue)
+        {
+            const globalValue = '123213123';
+            console.log(globalValue)
+        }
+        console.log(globalValue)
+    }
+    }
